650a8c390b6fbef25fd6ebf39d80509138be6a72
ankit026 <ankitsaxena261997@gmail.com>
2018-06-20 15:27:47 +0530

    Add comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
-	resources :posts
-	root "posts#index"
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #devise_for :users
+  resources :posts do
+    resources :comments
+  end
+  root "posts#index"
+  
+  get '/about', to: 'pages#about'
 end
